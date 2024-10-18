ec9fa8f86aeabd8f3bff8b5dd25b57ab01477d57
ahmad-masud <ahmadmasud25@hotmail.com>
2024-10-17 19:19:18 -0700

    updated finanical infrastructure

--- src/components/solution.js
+++ src/components/solution.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism'; // Import both styles
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import '../styles/solution.css'; // For custom modal styling
 
 const Solution = ({ isOpen, onClose, problemName }) => {
@@ -29,14 +30,13 @@ const Solution = ({ isOpen, onClose, problemName }) => {
     }
   }, [problemName, isOpen]);
 
-  // Handle code copy
-  const handleCopyCode = () => {
-    navigator.clipboard.writeText(code)
-      .then(() => {
-        setIsCopied(true);
-        setTimeout(() => setIsCopied(false), 2000); // Reset after 2 seconds
-      })
-      .catch(() => alert('Failed to copy the code'));
+  const handleCopy = () => {
+    setIsCopied(true);
+
+    // Reset the copied state after 2 seconds
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2000);
   };
 
   // Do not render the modal if it's not open
@@ -53,9 +53,6 @@ const Solution = ({ isOpen, onClose, problemName }) => {
     <div className="modal-overlay">
       <div className='overlay-backdrop' onClick={onClose}></div>
       <div className="modal-content">
-        <button className="copy-button" onClick={handleCopyCode}>
-          <i className={`bi bi-${isCopied ? 'check-lg' : 'copy'}`}></i>
-        </button>
         <button className="close-button" onClick={onClose}>
           <i className="bi bi-x"></i>
         </button>
@@ -63,9 +60,18 @@ const Solution = ({ isOpen, onClose, problemName }) => {
           <p>{error}</p>
         ) : (
           <div className="code-container">
-            <SyntaxHighlighter 
-              language="python" 
-              style={theme === 'dark' ? materialDark : undefined} // Apply style only if dark
+            <CopyToClipboard text={code} onCopy={() => handleCopy()}>
+              <button className="solution-copy-button">
+                {isCopied ? (
+                  <i className="bi bi-check-lg"></i>
+                ) : (
+                  <i className="bi bi-copy"></i>
+                )}
+              </button>
+            </CopyToClipboard>
+            <SyntaxHighlighter
+              language={'python'}
+              style={theme === 'dark' ? materialDark : undefined}
             >
               {code}
             </SyntaxHighlighter>
--- src/pages/premium.js
+++ src/pages/premium.js
@@ -3,19 +3,18 @@ import { useStripe } from '@stripe/react-stripe-js';
 import { getFunctions, httpsCallable } from 'firebase/functions';
 import { doc, getDoc } from 'firebase/firestore'; // To fetch user data from Firestore
 import { firestore } from '../config/firebase-config'; // Import Firestore instance
-import Confirm from '../components/confirm';
 import '../styles/premium.css';
 import { useAlerts } from '../context/alertscontext';
 
 const PremiumPage = ({ user }) => {
   const stripe = useStripe();
   const functions = getFunctions();
-  const [displayConfirm, setDisplayConfirm] = useState(false);
-  const [confirmMessage, setConfirmMessage] = useState('');
   const [premiumInfo, setPremiumInfo] = useState({
     premium: false,
     subscriptionId: '',
     canceled: false,
+    customerId: '',
+    subscriptionEnd: null,
   });
   const { addAlert } = useAlerts();
 
@@ -33,6 +32,8 @@ const PremiumPage = ({ user }) => {
                 premium: userData.premiumInfo.premium || false,  // Set a default value if undefined
                 subscriptionId: userData.premiumInfo.subscriptionId || "",  // Set default empty string if undefined
                 canceled: userData.premiumInfo.canceled || false,  // Set default to false if undefined
+                customerId: userData.premiumInfo.stripeCustomerId || "",  // Set default empty string if undefined
+                subscriptionEnd: userData.premiumInfo.subscriptionEnd || null,  // Set default to null if undefined
               });
             }
           }
@@ -44,35 +45,30 @@ const PremiumPage = ({ user }) => {
     }
   }, [user, addAlert]);
   
-
-  const handleCancelSubscription = async () => {
-    if (!premiumInfo.subscriptionId) {
-      console.log('No active subscription to cancel.');
-      addAlert('No active subscription to cancel.', 'warning');
+  const handleManageSubcription = async () => {
+    if (!user) {
+      console.error('User not logged in');
+      addAlert('User not logged in!', 'error');
       return;
     }
 
-    const cancelSubscription = httpsCallable(functions, 'cancelSubscription');
-
-    try {
-      const result = await cancelSubscription({ subscriptionId: premiumInfo.subscriptionId, uid: user.uid });
-
-      if (result.data.status === 'success') {
-        console.log('Subscription canceled successfully.');
-        addAlert('Subscription canceled successfully.', 'success');
-        setPremiumInfo({
-          ...premiumInfo,
-          canceled: true
-        });
-      } else {
-        console.error('Failed to cancel subscription.');
-        addAlert('Failed to cancel subscription.', 'error');
-      }
-    } catch (error) {
-      console.error('Error canceling subscription:', error);
-      addAlert('Error canceling subscription.', 'error');
+    if (!premiumInfo.customerId) {
+      console.error('Customer ID not found.');
+      addAlert('Customer ID not found!', 'error');
+      return;
     }
-  };
+    addAlert('Loading customer portal...', 'info');
+    const createCustomerPortalSession = httpsCallable(functions, 'createCustomerPortalSession');
+  
+    try {
+      const { data } = await createCustomerPortalSession({ customerId: premiumInfo.customerId }); // Make sure you have the user's Stripe customer ID stored in Firestore
+  
+      window.location.href = data.url; // Redirect to the Stripe Customer Portal
+    } catch (error) {
+      console.error('Error redirecting to customer portal:', error);
+      addAlert('Error redirecting to customer portal!', 'error');
+    }
+  };  
 
   const handlePremium = async (priceId, isSubscription) => {
     if (!user) {
@@ -81,9 +77,7 @@ const PremiumPage = ({ user }) => {
       return;
     }
 
-    if (premiumInfo.premium && !premiumInfo.canceled) {
-      setConfirmMessage('Are you sure you want to cancel your subscription?');
-      setDisplayConfirm(true);
+    if (premiumInfo.premium) {
       return;
     }
 
@@ -102,15 +96,8 @@ const PremiumPage = ({ user }) => {
     }
   };
 
-  const handleConfirm = () => {
-    addAlert('Cancelling subscription...');
-    handleCancelSubscription();
-    setDisplayConfirm(false);
-  }
-
   return (
     <div className="premium-page">
-      {displayConfirm && <Confirm onClose={() => setDisplayConfirm(false)} message={confirmMessage} onConfirm={handleConfirm}/>}
       <p className='premium-title'>Premium</p>
       <p className='premium-info'>Choose your subscription plan:</p>
       <div className="subscription-options">
@@ -120,7 +107,7 @@ const PremiumPage = ({ user }) => {
           <button
             className="checkout-button disabled"
           >
-            {premiumInfo.premium && !premiumInfo.canceled ? 'Previous Plan' : 'Current Plan'}
+            {premiumInfo.premium ? 'Previous Plan' : 'Current Plan'}
           </button>
           <div className='subscription-features'>
             <p className='subscription-feature'><i className="fa-solid fa-check"></i> Company-wise problems</p>
@@ -132,18 +119,28 @@ const PremiumPage = ({ user }) => {
           <p className='subscription-title'>Premium</p>
           <p className='subscription-cost'>$5<span>USD | month</span></p>
           <button
-            className={"checkout-button" + (premiumInfo.premium && !premiumInfo.canceled ? ' cancel' : '')}
+            className={"checkout-button" + (premiumInfo.premium ? ' disabled' : '')}
             onClick={() => handlePremium("price_1Q9xzfFyVXgjLrCzwNw8Hrxc", true)}
           >
-            {premiumInfo.premium && !premiumInfo.canceled ? 'Cancel Premium' : 'Upgrade to Premium'}
+            {premiumInfo.premium ? 'Current Plan' : 'Upgrade to Premium'}
           </button>
           <div className='subscription-features'>
-          <p className='subscription-feature'><i className="fa-solid fa-check"></i> Company-wise problems</p>
+            <p className='subscription-feature'><i className="fa-solid fa-check"></i> Company-wise problems</p>
             <p className='subscription-feature'><i className="fa-solid fa-check"></i> Data structure problems and solutions</p>
             <p className='subscription-feature'><i className="fa-solid fa-check"></i> Algorithm problems and solutions</p>
             <p className='subscription-feature'><i className="fa-solid fa-check"></i> Frequency count for company-wise problems</p>
             <p className='subscription-feature'><i className="fa-solid fa-check"></i> Solutions for company problems</p>
           </div>
+          {premiumInfo.premium && (
+            <button className='manage-button' onClick={handleManageSubcription}>
+              Manage my subscription
+            </button>
+          )}
+          {premiumInfo.premium && premiumInfo.canceled && premiumInfo.subscriptionEnd && (
+            <p className='cancel-info'>
+              You will have access to premium until {premiumInfo.subscriptionEnd.toDate().toLocaleDateString()}.
+            </p>
+          )}
         </div>
       </div>
     </div>
