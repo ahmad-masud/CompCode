3bc68af1967a3e6cc119385ca5c9d4c1f21e4cb2
ahmad-masud <ahmadmasud25@hotmail.com>
2024-10-20 14:17:43 -0700

    updates

--- src/components/solution.js
+++ src/components/solution.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism'; // Import both styles
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { useAlerts } from '../context/alertscontext';
 import '../styles/solution.css'; // For custom modal styling
 
 const Solution = ({ isOpen, onClose, problemName, solution }) => {
@@ -9,10 +10,12 @@ const Solution = ({ isOpen, onClose, problemName, solution }) => {
   const [error, setError] = useState('');
   const [isCopied, setIsCopied] = useState(false); // State for handling copy feedback
   const [loading, setLoading] = useState(true);
+  const { addAlert } = useAlerts();
 
   // Fetch the code from GitHub when the modal opens
   useEffect(() => {
     setLoading(true);
+    setError('');
     if (solution && isOpen) {
       setCode(solution);
     }
@@ -23,19 +26,20 @@ const Solution = ({ isOpen, onClose, problemName, solution }) => {
             `https://raw.githubusercontent.com/kamyu104/LeetCode-Solutions/master/Python/${problemName}.py`
           );
           if (!response.ok) {
-            throw new Error('Problem not found');
+            throw new Error('Solution not found');
           }
           const data = await response.text();
           setCode(data);
           setLoading(false);
         } catch (error) {
           setError(error.message);
+          addAlert('No solution yet', 'warning');
           setLoading(false);
         }
       };
       fetchCode();
     }
-  }, [problemName, isOpen, solution]);
+  }, [problemName, isOpen, solution, addAlert]);
 
   const handleCopy = () => {
     setIsCopied(true);
@@ -47,7 +51,7 @@ const Solution = ({ isOpen, onClose, problemName, solution }) => {
   };
 
   // Do not render the modal if it's not open
-  if (!isOpen || loading) return null;
+  if (!isOpen || error || loading) return null;
 
   // Get the theme from local storage
   var theme = localStorage.getItem('theme') || 'auto'; // Default to 'auto' if not set
@@ -63,27 +67,23 @@ const Solution = ({ isOpen, onClose, problemName, solution }) => {
         <button className="close-button" onClick={onClose}>
           <i className="bi bi-x"></i>
         </button>
-        {error ? (
-          <p>{error}</p>
-        ) : (
-          <div className="code-container">
-            <CopyToClipboard text={code} onCopy={() => handleCopy()}>
-              <button className="solution-copy-button">
-                {isCopied ? (
-                  <i className="bi bi-check-lg"></i>
-                ) : (
-                  <i className="bi bi-copy"></i>
-                )}
-              </button>
-            </CopyToClipboard>
-            <SyntaxHighlighter
-              language={'python'}
-              style={theme === 'dark' ? materialDark : undefined}
-            >
-              {code}
-            </SyntaxHighlighter>
-          </div>
-        )}
+        <div className="code-container">
+          <CopyToClipboard text={code} onCopy={() => handleCopy()}>
+            <button className="solution-copy-button">
+              {isCopied ? (
+                <i className="bi bi-check-lg"></i>
+              ) : (
+                <i className="bi bi-copy"></i>
+              )}
+            </button>
+          </CopyToClipboard>
+          <SyntaxHighlighter
+            language={'python'}
+            style={theme === 'dark' ? materialDark : undefined}
+          >
+            {code}
+          </SyntaxHighlighter>
+        </div>
       </div>
     </div>
   );
