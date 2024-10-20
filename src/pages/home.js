f970cb8e3b650c47fcab29ae5d6c7b6bd6148d24
ahmad-masud <ahmadmasud25@hotmail.com>
2024-10-19 17:16:05 -0700

    merged algorithms and data structures into a roadmap along with lessons for each topic

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,8 +2,14 @@ import React from 'react';
 import '../styles/home.css';
 import { Link } from 'react-router-dom';
 import ReleaseNotes from '../components/release';
+import tips from '../content/tips';
 
 const Home = () => {
+    const today = new Date();
+    const dayOfYear = Math.floor((today - new Date(today.getFullYear(), 0, 0)) / 86400000);
+    const tipIndex = dayOfYear % tips.length;
+    const tip = tips[tipIndex];
+
     return (
         <div className='home'>
             <div className='hero'>
@@ -11,6 +17,7 @@ const Home = () => {
                     <p className='hero-title'><span><i className="fa-solid fa-meteor"></i> CompCode:</span> The best resource for doing LeetCode Problems</p>
                     <p className='hero-overview'>Prepare for your company's technical interview through solving frequently asked LeetCode problems.</p>
                     <Link to="/companies" className='hero-link'>Get Started</Link>
+                    <p className='tech-tip'>Today's tech tip: {tip}</p>
                 </div>
             </div>
             <ReleaseNotes />
