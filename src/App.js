33c3499562de7852ab290653442126589393177d
ahmad-masud <ahmadmasud25@hotmail.com>
2024-10-21 19:57:23 -0700

    added lessons page

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import Roadmap from './pages/roadmap';
 import lessons from './content/lessons.json';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { firestore } from './config/firebase-config';
+import Lessons from './pages/lessons';
 
 const stripePromise = loadStripe(String(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY));
 
@@ -120,6 +121,8 @@ const App = () => {
               <Route path='/' element={<Home />} />
               <Route path='/companies' element={<Companies theme={theme} user={user} premiumInfo={premiumInfo} />} />
               <Route path='/roadmap' element={<Roadmap theme={theme} user={user} />} />
+              <Route path='/lesson' element={<NotFound />} />
+              <Route path='/lessons' element={<Lessons lessons={lessons} />} />
               <Route path='/premium' element={<Premium user={user} premiumInfo={premiumInfo} />} />
               {lessons.map((lesson, index) => (
                 <Route key={`${lesson.id}-${index}`} path={`/lesson/${lesson.title.replaceAll(' ', '-').toLowerCase()}`} element={<Lesson data={lesson} theme={theme} />} />
---
+++ src/pages/lessons.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import '../styles/lessons.css';
+import { useNavigate } from 'react-router-dom';
+
+const Lessons = ({ lessons }) => {
+  const navigate = useNavigate();
+  const categorizedLessons = {
+    dataStructures: [],
+    algorithms: [],
+    concepts: [],
+  };
+
+  // Loop through all lessons and categorize them based on type
+  lessons.forEach((lesson) => {
+    if (lesson.type === 'datastructure') {
+      categorizedLessons.dataStructures.push(lesson);
+    } else if (lesson.type === 'algorithm') {
+      categorizedLessons.algorithms.push(lesson);
+    } else if (lesson.type === 'concept') {
+      categorizedLessons.concepts.push(lesson);
+    }
+  });
+
+  // Helper function to format the image path based on the lesson title
+  const getLessonImagePath = (title) => {
+    const formattedTitle = title.replace(/\s|-/g, '').toLowerCase();
+    return require(`../content/images/${formattedTitle}.webp`);
+  };
+
+  const renderLessons = (category, title) => (
+    <div key={title}>
+      <p className='lesson-type-title'>{title}</p>
+      <div className="lessons-container">
+        {category.map((lesson, index) => (
+          <div onClick={() => navigate(`/lesson/${lesson.title.replaceAll(' ', '-').toLowerCase()}`)} key={index} className="lesson-card">
+            <img className="lesson-card-image" src={getLessonImagePath(lesson.title)} alt={lesson.title} />
+            <div className="lesson-card-text">
+                <p className="lesson-card-title" >{lesson.title}</p>
+                <div className="lesson-card-sub-text">
+                    <p className='lesson-card-duration'>{lesson.duration || 'N/A'}</p>
+                    <p className={'lesson-card-difficulty ' + lesson.difficulty}>{lesson.difficulty[0].toUpperCase() + lesson.difficulty.slice(1) || 'N/A'}</p>
+                </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
+  return (
+    <div className='lesson-types'>
+      {renderLessons(categorizedLessons.dataStructures, 'Data Structures')}
+      {renderLessons(categorizedLessons.algorithms, 'Algorithms')}
+      {renderLessons(categorizedLessons.concepts, 'Concepts')}
+    </div>
+  );
+};
+
+export default Lessons;
